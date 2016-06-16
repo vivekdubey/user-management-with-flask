69b0b6f2f541b481719a08156c0431f0016bb39f
Vivek Dubey <dubeyv@tabcorp.com.au>
2016-06-17 00:11:46 +1000

    Added migration folder to gitignore and add is_admin column in users table

--- models.py
+++ models.py
@@ -5,16 +5,18 @@ from sqlalchemy.dialects.postgresql import JSON
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String())
-    name = db.Column(db.String())
+    email = db.Column(db.String(120), index=True, unique=True)
+    name = db.Column(db.String(120), index=True, unique=True)
     password = db.Column(db.String)
+    is_admin = db.Column(db.Boolean)
     created_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     updated_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
-    def __init__(self, email, name, password):
+    def __init__(self, email, name, password, is_admin):
         self.email = email
         self.name = name
         self.password = password
+        self.is_admin = is_admin
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
